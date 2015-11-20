f558e372bcf4e4fdc8b1f622040d4246c8c3d5f2
Randy Chen <goodjob1114@gmail.com>
2015-11-20 17:06:00 +0800

    add some filetype support

--- lab/vmp3.rb
+++ lab/vmp3.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 workdir = ARGV[0] || '.'
-Dir.glob("#{workdir}/**/*.{mkv,MKV,mp4}").each { |file| puts `ffmpeg -i '#{file}' -y '#{file.sub(/\.\w+$/, ".mp3")}'`}
+Dir.glob("#{workdir}/**/*.{mkv,MKV,mp4,avi,webm}").each { |file| puts `ffmpeg -i '#{file}' -y '#{file.sub(/\.\w+$/, ".mp3")}'`}
