2883885b15b71b58b834b201e122fd7c75651290
Randy Chen <goodjob1114@gmail.com>
2015-11-24 12:27:12 +0800

    quotation marks bug in shell

--- lab/vmp3.rb
+++ lab/vmp3.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 workdir = ARGV[0] || '.'
-Dir.glob("#{workdir}/**/*.{mkv,MKV,mp4,webm}").each { |file| mp3file=file.sub(/\.\w+$/, ".mp3"); puts `ffmpeg -i '#{file}' -y '#{mp3file}'` if !File.exist?(mp3file)}
+Dir.glob("#{workdir}/**/*.{mkv,MKV,mp4,webm}").each { |file| file.gsub!(/'/, "'\\\\''"); mp3file=file.sub(/\.\w+$/, ".mp3"); puts `ffmpeg -i '#{file}' -y '#{mp3file}'` if !File.exist?(mp3file)}
